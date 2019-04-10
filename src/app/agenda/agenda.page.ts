697bb5c125acd5846f406d03e8393b2944c9a228
davithc01@gmail.com <AngelaM2019>
2019-04-09 23:48:53 -0500

    working in Graphql

---
+++ src/app/agenda/agenda.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../orders.service';
+
+
+@Component({
+  selector: 'app-agenda',
+  templateUrl: './agenda.page.html',
+  styleUrls: ['./agenda.page.scss'],
+})
+export class AgendaPage implements OnInit {
+
+  orders: any;
+  
+  constructor(public ordersService:OrdersService) { }
+
+  ngOnInit() {
+    const _self = this;
+    const onSuccess = function(ordersList) {
+      console.log(ordersList);
+      _self.orders = ordersList;
+    }
+    const onError = function(error) {}
+    this.ordersService.getOrderList(onSuccess,onError);
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloModule, Apollo } from 'apollo-angular';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    HttpLinkModule,
+    ApolloModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    apollo.create({
+      link: httpLink.create({ uri: 'http://tester.estrategicacomunicaciones.com/graphql' }),
+      cache: new InMemoryCache()
+    });
+  }
+}
