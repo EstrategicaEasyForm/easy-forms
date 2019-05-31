c05cc105cb5839a3c207fd7a43aeb86e6ad023d2
EstrategicaEasyForm <estrategica.easy.form@gmail.com>
2019-05-31 16:05:02 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,10 +13,10 @@ const routes: Routes = [
   { path: 'aspiration', loadChildren: './aspiration/aspiration.module#AspirationPageModule' },
   { path: 'aspiration-detail', loadChildren: './aspiration/aspiration-detail.module#AspirationDetailPageModule' },
   { path: 'transfer', loadChildren: './transfer/transfer.module#TransferPageModule' },
-  //{ path: 'transfer-detail', loadChildren: './transfer/transfer-detail.module#TransferDetailPageModule' },
   { path: 'diagnostic', loadChildren: './diagnostic/diagnostic.module#DiagnosticPageModule' },
   { path: 'diagnostic-detail', loadChildren: './diagnostic/diagnostic-detail.module#DiagnosticDetailPageModule' },
   { path: 'sexage', loadChildren: './sexage/sexage.module#SexagePageModule' },
+  { path: 'sexage-detail', loadChildren: './sexage/sexage-detail.module#SexageDetailPageModule' },
   { path: 'delivery', loadChildren: './delivery/delivery.module#DeliveryPageModule' },
   { path: 'transfer-detail', loadChildren: './transfer/transfer-detail.module#TransferDetailPageModule' }
 
